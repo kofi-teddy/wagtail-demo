8885fe1b69cc23e5de7ec94d29639e36b12c1fa1
kofi-teddy <agudogot@gmail.com>
2022-01-30 20:55:07 +0000

    projects config

---
+++ core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-r7#@b(h4xc^^vp$v-#_xq)24*o##z#j6f!$%rgwhm_c2oo4+^b'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
